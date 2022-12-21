4c3eccc957d213f1801e1ee2cc3e364470108c8f
Hoang3409 <nvh.02021995@gmail.com>
2022-12-21 23:55:22 +0700

    Implement class Diem, HocSinh

---
+++ LTWindows/Core/Diem.cs
@@ -0,0 +1,42 @@
+﻿namespace LTWindows.Core
+{
+    internal class Diem
+    {
+        private double _Toan;
+        private double _Ly;
+        private double _Hoa;
+        private double _Tb;
+
+        public Diem(double toan, double ly, double hoa)
+        {
+            Toan = toan;
+            Ly = ly;
+            Hoa = hoa;
+            _Tb = (_Toan + _Ly + _Hoa) / 3;
+        }
+
+        public double Toan
+        {
+            get { return _Toan; }
+            set { _Toan = value; _Tb = (_Toan + _Ly + _Hoa) / 3; }
+        }
+
+        public double Ly
+        {
+            get { return _Ly; }
+            set { _Ly = value; _Tb = (_Toan + _Ly + _Hoa) / 3; }
+        }
+
+        public double Hoa
+        {
+            get { return _Hoa; }
+            set { _Hoa = value; _Tb = (_Toan + _Ly + _Hoa) / 3; }
+        }
+
+        public double Tb
+        {
+            get { return _Tb; }
+        }
+
+    }
+}
---
+++ LTWindows/Core/HocSinh.cs
@@ -0,0 +1,42 @@
+﻿namespace LTWindows.Core
+{
+    internal class HocSinh : Diem
+    {
+        private string maHV;
+        private string hoTen;
+        private string phai;
+        private string soDT;
+
+        public HocSinh(string maHV, string hoTen, string phai, string soDT, double toan, double hoa, double ly) : base(toan, hoa, ly)
+        {
+            this.maHV = maHV;
+            this.hoTen = hoTen;
+            this.phai = phai;
+            this.soDT = soDT;
+        }
+
+        public string MaHV
+        {
+            get { return maHV; }
+            set { maHV = value; }
+        }
+
+        public string HoTen
+        {
+            get { return hoTen; }
+            set { hoTen = value; }
+        }
+
+        public string Phai
+        {
+            get { return phai; }
+            set { phai = value; }
+        }
+
+        public string SoDT
+        {
+            get { return soDT; }
+            set { soDT = value; }
+        }
+    }
+}
