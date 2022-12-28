f1c76816c888e804dfff80afd7889749d1af9275
Hoang3409 <nvh.02021995@gmail.com>
2022-12-28 08:21:29 +0700

    Init

--- LTWindows/MainView.cs
+++ LTWindows/MainView.cs
@@ -1,5 +1,3 @@
-using System.Drawing.Drawing2D;
-
 namespace LTWindows
 {
     public partial class MainView : Form
@@ -7,36 +5,6 @@ namespace LTWindows
         public MainView()
         {
             InitializeComponent();
-            DoubleBuffered = true;
-        }
-
-        // Setup linear gradient background
-        protected override void OnPaintBackground(PaintEventArgs e)
-        {
-            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
-                                                               Color.Gray,
-                                                               Color.Black,
-                                                               45F))
-            {
-                e.Graphics.FillRectangle(brush, this.ClientRectangle);
-            }
-        }
-
-        private void btnBai_Click(object sender, EventArgs e)
-        {
-            string className = ((Button)sender).Name.Split("btn")[1];
-            string objectToInstantiate = "LTWindows." + className + ", LTWindows";
-
-            Type? objectType = Type.GetType(objectToInstantiate);
-            Form? instantiatedObject = (Form)Activator.CreateInstance(objectType, this);
-            if (instantiatedObject == null)
-            {
-                MessageBox.Show("Error");
-                return;
-            }
-            instantiatedObject.Show();
-
-            this.Hide();
         }
     }
 }
